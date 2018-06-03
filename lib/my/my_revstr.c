ae95b1ba9b6092f31b0acb42f6d6dbc8d4705762
lucas.tabis <lucas.tabis@localhost.localdomain>
2018-06-03 17:49:52 +0200

    [updated] minor fix for putstr and puterr, optimized revstr

--- lib/my/my_revstr.c
+++ lib/my/my_revstr.c
@@ -5,14 +5,14 @@
 ** task03 Day06
 */
 
+#include "my.h"
+
 char *my_revstr(char *str)
 {
-	unsigned int i = 0;
-	int count = 0;
+	unsigned int i = strlen(str);
 	char save = 0;
 
-	for (; str[i] != 0; i++);
-	for (; count != i / 2; count++) {
+	for (unsigned int count = 0; count != i / 2; count++) {
 		save = str[count];
 		str[count] = str[i - count - 1];
 		str[i - count - 1] = save;
