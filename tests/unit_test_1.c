36a438011d3697b45462d42ac73a0a41eecb9a9f
lucas.tabis <lucas.tabis@localhost.localdomain>
2018-06-03 22:51:43 +0200

    [updated] added only one unit test

--- tests/unit_test_1.c
+++ tests/unit_test_1.c
@@ -14,3 +14,20 @@ void redirect_all_std(void)
 	cr_redirect_stdout();
 	cr_redirect_stderr();
 }
+
+Test(lib_tests, concat)
+{
+	char *concat = my_concat("I am a", " test!");
+	char *concat2 = my_concat(NULL, "test");
+	char *concat3 = my_concat("test", NULL);
+	char *concat4 = my_concat(NULL, NULL);
+	char *concat5 = my_concat("test", "\1");
+
+	cr_assert_str_eq(concat, "I am a test!");
+	cr_assert_eq(concat2, NULL);
+	cr_assert_eq(concat3, NULL);
+	cr_assert_eq(concat4, NULL);
+	cr_assert_str_eq(concat5, "test\1");
+	free(concat);
+	free(concat5);
+}
