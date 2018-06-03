0bb58dc959dbe9c1d50aa3c03f3a08e489ac3037
lucas.tabis <lucas.tabis@localhost.localdomain>
2018-06-03 17:40:53 +0200

    [update] minor changes for my_isnum function (checking if string NULL)

--- lib/my/my_isnum.c
+++ lib/my/my_isnum.c
@@ -1,8 +1,8 @@
 /*
 ** EPITECH PROJECT, 2017
-** my_str_isum.c
+** my_isum.c
 ** File description:
-** search if a string is num or not
+** search if a string is num or not (returns -1 if not / 0 if num)
 */
 
 int my_isnum(const char *str)
@@ -10,9 +10,11 @@ int my_isnum(const char *str)
 	unsigned int i = 0;
 	unsigned int nbr = 0;
 
+	if (!str)
+		return (-1);
 	i = str[0] == '-' ? 1 : 0;
 	for (; str[i] != 0; i++)
 		if (str[i] < '0' || str[i] > '9')
-			return (1);
+			return (-1);
 	return (0);
 }
