678836ecac9d7c591a80f20ff7716f4f3e49201a
lucas.tabis <lucas.tabis@localhost.localdomain>
2018-06-25 15:48:10 +0200

    retreived strncmp by my_strncmp in my_getenv function

--- lib/my/my_getenv.c
+++ lib/my/my_getenv.c
@@ -11,7 +11,7 @@ int my_getenv(const char *variable, char * const *env)
 {
 	for (int index = 0, len = my_strlen(variable);
 	env && env[index]; index++)
-		if (strncmp(env[index], variable, len) == 0)
+		if (my_strncmp(env[index], variable, len - 1) == 0)
 			return (index);
 	return (-1);
 }
