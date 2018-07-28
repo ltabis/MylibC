b2f816c6513c7536225c9470a6fd7be9e65e1b24
lucas.tabis <lucas.tabis@localhost.localdomain>
2018-07-28 17:40:31 +0200

    Added my_isalpha simple function

---
+++ lib/my/my_isalpha.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2017
+** my_isum.c
+** File description:
+** search if a string contain alpha caracters or not (returns -1 if not / 0 if true)
+*/
+
+int my_isalpha(const char *str)
+{
+	if (!str)
+		return (-1);
+	for (unsigned int i = 0; str[i] != 0; i++)
+		if ((str[i] < 'a' || str[i] > 'z') &&
+		(str[i] < 'A' || str[i] > 'Z'))
+			return (-1);
+	return (0);
+}
