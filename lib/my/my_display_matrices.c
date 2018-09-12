bf8dc1d0b6c391d00e7619b48e100d0e7d842c81
Lucas Tabis <lucas.tabis@epitech.eu>
2018-09-12 06:54:48 +0200

    Modified display functions to display floats (WIP).

--- lib/my/my_display_matrices.c
+++ lib/my/my_display_matrices.c
@@ -7,7 +7,24 @@
 
 #include "my.h"
 
-void display_integer_matrix(int **matrix, int col, int row)$
+/* Functions mainly used for debugging */
+
+void display_integer_matrix(int **matrix, int col, int row)
+{
+	if (!matrix)
+		return;
+	for (unsigned int i = 0; i < col; i++) {
+		if (!matrix[i])
+			return;
+		for (unsigned int i = 0; j < row; i++) {
+			my_put_nbr(matrix[i][j]);
+			my_putchar(' ');
+		}
+		my_putchar('\n');
+	}
+}
+
+void display_float_matrix(float **matrix, int col, int row)
 {
 	if (!matrix)
 		return;
@@ -15,6 +32,6 @@ void display_integer_matrix(int **matrix, int col, int row)$
 		if (!matrix[i])
 			return;
 		for (unsigned int i = 0; j < row; i++)
-			my_put_nbr(matrix[i][j]);
+			printf(matrix[i][j]);
 	}
 }
