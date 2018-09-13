2f08cbe1554ba22b7456b574d679228d073db332
Lucas Tabis <lucas.tabis@epitech.eu>
2018-09-13 06:37:01 +0200

    Added more unit tests for matrices.

--- tests/unit_test_matrix_1.c
+++ tests/unit_test_matrix_1.c
@@ -16,6 +16,35 @@ Test(lib_tests, matrix_generation_integer)
 	for (unsigned int i = 0; i < 5; i++)
 		for (unsigned int j = 0; j < 5; j++)
 			cr_assert_eq(matrix[i][j], 10);
-	delete_mundane_matrix((void **)matrix, 5, 5);
-	cr_assert_eq(matrix, NULL);
+	delete_mundane_matrix((void **)matrix, 5);
+}
+
+Test(lib_tests, matrix_generation_double)
+{
+	double **matrix = generate_double_matrix(10, 10, 92.3);
+
+	for (unsigned int i = 0; i < 10; i++)
+		for (unsigned int j = 0; j < 10; j++)
+			cr_assert_eq(matrix[i][j], 92.3);
+	delete_mundane_matrix((void **)matrix, 10);
+}
+
+Test(lib_tests, matrix_generation_float)
+{
+	float **matrix = generate_float_matrix(7, 7, 9.452f);
+
+	for (unsigned int i = 0; i < 7; i++)
+		for (unsigned int j = 0; j < 7; j++)
+			cr_assert_eq(matrix[i][j], 9.452f);
+	delete_mundane_matrix((void **)matrix, 7);
+}
+
+Test(lib_tests, matrix_generation_char)
+{
+	int **matrix = generate_integer_matrix(50, 100, 'o');
+
+	for (unsigned int i = 0; i < 50; i++)
+		for (unsigned int j = 0; j < 100; j++)
+			cr_assert_eq(matrix[i][j], 'o');
+	delete_mundane_matrix((void **)matrix, 50);
 }
