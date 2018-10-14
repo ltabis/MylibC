cc07bfb91335021fae403f6ae43c4da604737c10
Lucas Tabis <lucas.tabis@epitech.eu>
2018-10-14 12:57:57 +0200

    Updated rev string functions.

--- lib/my/my_revstr.c
+++ lib/my/my_revstr.c
@@ -6,12 +6,16 @@
 */
 
 #include "my.h"
+#include <stdio.h>
 
 char *my_revstr(char *str)
 {
-	unsigned int i = my_strlen(str);
+	unsigned int i = 0;
 	char save = 0;
 
+	if (!str)
+		return (NULL);
+	i = my_strlen(str);
 	for (unsigned int count = 0; count != i / 2; count++) {
 		save = str[count];
 		str[count] = str[i - count - 1];
--- lib/my/my_strrevcmp.c
+++ lib/my/my_strrevcmp.c
@@ -12,6 +12,8 @@ int my_strrevcmp(const char *s1, const char *s2)
 	unsigned int count = my_strlen(s1);
 	unsigned int count2 = my_strlen(s2);
 
+	if (!s1 || !s2)
+		return (-1);
 	for (unsigned int i = 0; s1[count] == s2[count2] && count != 0
 	&& count2 != 0; i++) {
 		count--;
